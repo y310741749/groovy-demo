7d6ec17f62a235f400e530fbe2878b8788bc8061
杨鹏 <310741749@qq.com>
2023-03-12 17:38:27 +0800

    测试提交

--- src/com/demo10.groovy
+++ src/com/demo10.groovy
@@ -3,6 +3,7 @@ package com
 class demo10 {
     public static void main(String[] args) {
         def list=["love","you"]
+
         println list.inject("I"){
             sum,elem->"${sum} ${elem}"
         }
